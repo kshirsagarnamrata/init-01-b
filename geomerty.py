8b83baa88b6335f4440184a38eb83590556dd452
Namrata Kshirsagar <kshirsagarnamrata24@gmail.com>
2025-12-10 21:25:29 +0530

    geomerty

---
+++ geomerty.py
@@ -0,0 +1,7 @@
+def area(a,b):
+    print("area of reactangle=",a*b)
+
+def peri(a,b):
+    print("perimeter of reactangle=",2(a*b))
+
+
--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 import arithmeric
+import geomerty as geo
 print("hello word")
+arithmeric.add(10,20)
+arithmeric.substract(10,20)
+geo.area(10,20)
+geo.peri(10,20)
